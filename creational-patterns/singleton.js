6f0371e278546435cf90dec3f36276e478a42919
LamDoan <thanhlam.fit@gmail.com>
2018-03-25 10:43:53 +0700

    singleton pattern

---
+++ creational-patterns/singleton.js
@@ -0,0 +1,29 @@
+const singleton = Symbol();
+const singletonEnforcer = Symbol();
+
+class SoundManager {
+  constructor(enforcer) {
+    if (enforcer !== singletonEnforcer) {
+      throw new Error('Cannot use constructor');
+    }
+  }
+
+  static getInstance() {
+    if (!this[singleton]) {
+      this[singleton] = new SoundManager(singletonEnforcer);
+    }
+    return this[singleton];
+  }
+
+  loadResource(url) {
+    return url;
+  }
+
+  play() {
+    return 'played';
+  }
+
+  stop() {
+    return 'stopped';
+  }
+}
